f08f82239fe7f0953d3cb133bfa5eaf16e479810
Mahathi <mtb.violin@gmail.com>
2024-12-10 18:12:05 +0530

    cygwin_build.sh updated

--- Tools/scripts/cygwin_build.sh
+++ Tools/scripts/cygwin_build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # script to build cygwin binaries for using in MissionPlanner
 # the contents of artifacts directory is uploaded to:
@@ -7,39 +7,16 @@
 # the script assumes you start in the root of the ardupilot git tree
 
 set -x
-set -e
-
-# TOOLCHAIN=i686-pc-cygwin
-TOOLCHAIN=x86_64-pc-cygwin
-GPP_COMPILER="${TOOLCHAIN}-g++"
-
-$GPP_COMPILER -print-sysroot
-
-SYS_ROOT=$($GPP_COMPILER -print-sysroot)
-echo "SYS_ROOT=$SYS_ROOT"
 
 rm -rf artifacts
 mkdir artifacts
 
-# cygwin doesn't work out the parallelism properly
-WAF_OPTIONS="-j8"
-
 (
-    python ./waf --color yes --toolchain $TOOLCHAIN --board sitl configure 2>&1
-    python ./waf plane 2>&1
-    python ./waf copter 2>&1
-    python ./waf heli 2>&1
-    python ./waf rover 2>&1
-    python ./waf sub 2>&1
+    python ./waf --color yes --toolchain i686-pc-cygwin --board sitl configure 2>&1
+    python ./waf plane rover copter sub heli 2>&1
 ) | tee artifacts/build.txt
 
-# copy both with exe and without to cope with differences
-# between windows versions in CI
-cp -v build/sitl/bin/arduplane artifacts/ArduPlane.elf.exe
-cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf.exe
-cp -v build/sitl/bin/arducopter-heli artifacts/ArduHeli.elf.exe
-cp -v build/sitl/bin/ardurover artifacts/ArduRover.elf.exe
-cp -v build/sitl/bin/ardusub artifacts/ArduSub.elf.exe
+i686-pc-cygwin-g++ -print-sysroot
 
 cp -v build/sitl/bin/arduplane artifacts/ArduPlane.elf
 cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf
@@ -47,14 +24,6 @@ cp -v build/sitl/bin/arducopter-heli artifacts/ArduHeli.elf
 cp -v build/sitl/bin/ardurover artifacts/ArduRover.elf
 cp -v build/sitl/bin/ardusub artifacts/ArduSub.elf
 
-# Find all cyg*.dll files returned by cygcheck for each exe in artifacts
-# and copy them over
-for exe in artifacts/*.exe; do 
-    echo $exe
-    cygcheck $exe | grep -oP 'cyg[^\s\\/]+\.dll' | while read -r line; do
-      cp -v /usr/bin/$line artifacts/
-    done
-done
+cp -v /usr/i686-pc-cygwin/sys-root/usr/bin/*.dll artifacts/
 
 git log -1 > artifacts/git.txt
-ls -l artifacts/
