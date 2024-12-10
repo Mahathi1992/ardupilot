0989182f800e98e103385e5d2306fdd3e4a0eb8b
Mahathi <mtb.violin@gmail.com>
2024-12-10 20:30:37 +0530

    deleted all spaces

--- Tools/scripts/cygwin_build.sh
+++ Tools/scripts/cygwin_build.sh
@@ -1,28 +1,14 @@
 #!/usr/bin/env bash
-
-# script to build cygwin binaries for using in MissionPlanner
-# the contents of artifacts directory is uploaded to:
-# https://firmware.ardupilot.org/Tools/MissionPlanner/sitl/
-
-# the script assumes you start in the root of the ardupilot git tree
-
 set -x
 set -e
-
-# TOOLCHAIN=i686-pc-cygwin
 TOOLCHAIN=x86_64-pc-cygwin
 GPP_COMPILER="${TOOLCHAIN}-g++"
-
 $GPP_COMPILER -print-sysroot
-
 SYS_ROOT=$($GPP_COMPILER -print-sysroot)
 echo "SYS_ROOT=$SYS_ROOT"
-
 git config --global --add safe.directory /cygdrive/d/a/ardupilot/ardupilot
-
 rm -rf artifacts
 mkdir artifacts
-
 (
     python ./waf --color yes --toolchain $TOOLCHAIN --board sitl configure 2>&1
     python ./waf plane 2>&1
@@ -31,7 +17,6 @@ mkdir artifacts
     # python ./waf rover 2>&1
     # python ./waf sub 2>&1
 ) | tee artifacts/build.txt
-
 # copy both with exe and without to cope with differences
 # between windows versions in CI
 cp -v build/sitl/bin/arduplane artifacts/ArduPlane.elf.exe
