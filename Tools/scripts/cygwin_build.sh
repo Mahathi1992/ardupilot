357092feecab103ccecc8d57fa03903555b528a2
Mahathi Bhargavapuri <mtb.violin@gmail.com>
2024-12-12 14:20:37 +0530

    Update cygwin_build.sh

--- Tools/scripts/cygwin_build.sh
+++ Tools/scripts/cygwin_build.sh
@@ -2,9 +2,11 @@
 git config --global --add safe.directory /cygdrive/d/a/ardupilot/ardupilot
 rm -rf artifacts
 mkdir artifacts
-(python ./waf --color yes --toolchain i686-pc-cygwin --board sitl configure 2>&1
-python ./waf plane 2>&1
-python ./waf copter 2>&1) | tee artifacts/build.txt
+(
+  python ./waf --color yes --toolchain i686-pc-cygwin --board sitl configure 2>&1
+  python ./waf plane 2>&1
+  python ./waf copter 2>&1
+) | tee artifacts/build.txt
 i686-pc-cygwin-g++ -print-sysroot
 cp -v build/sitl/bin/arduplane artifacts/ArduPlane.elf.exe
 cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf.exe
