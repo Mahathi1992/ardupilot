ff3bc8d894ba428283d66b7408180c86391c0b67
Mahathi Bhargavapuri <mtb.violin@gmail.com>
2024-12-12 14:50:53 +0530

    Update cygwin_build.sh

--- Tools/scripts/cygwin_build.sh
+++ Tools/scripts/cygwin_build.sh
@@ -16,7 +16,7 @@ rm -rf artifacts
 mkdir artifacts
 
 (
-  python ./waf --color yes --toolchain i686-pc-cygwin --board sitl configure 2>&1
+  python ./waf --color yes --toolchain x86_64-pc-cygwin --board sitl configure 2>&1
   python ./waf plane 2>&1
   python ./waf copter 2>&1
 ) | tee artifacts/build.txt
